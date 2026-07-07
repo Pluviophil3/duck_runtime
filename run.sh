a4002f964d8bb950d8cf4efac10a3b60d1cd8896
Pluviophil3 <12212225@mail.sustech.edu.cn>
2026-07-07 15:15:46 +0800

    sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+cd ~/duck_runtime/scripts
+python v2_rl_walk_mjlab.py \
+  --onnx_model_path sway_t2_full.onnx \
+  --motion_path A2_-_Sway_t2_stageii.npz \
+  --duck_config_path ../duck_config.json \
+  --control_freq 50 \
+  --max_target_step 0.04 \
+  --debug
