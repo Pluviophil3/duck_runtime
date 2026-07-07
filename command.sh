f039eb21a4c411c60f9d110f5cc52a576f73235f
Pluviophil3 <12212225@mail.sustech.edu.cn>
2026-07-07 15:41:54 +0800

    sh

---
+++ command.sh
@@ -0,0 +1,7 @@
+python scripts/check_mjlab_initial_pose.py \
+  --pose motion_start \
+  --motion_path A2_-_Sway_t2_stageii.npz \
+  --duck_config_path ../duck_config.json \
+  --kp 2 \
+  --hold_seconds 5 \
+  --command
